bacfa95812eb57a79a61bdaf8729d0fe00b0bf7e
Andrew Coutts <andrewjohncoutts@gmail.com>
2018-12-07 22:24:38 -0500

    Disable CPU pegging

--- start.sh
+++ start.sh
@@ -6,6 +6,6 @@ echo -e "Starting Nodeos \n";
 ulimit -n 65535
 ulimit -s 64000
 
-cpufreq-set -c 0 -d 4.2GHz -g performance
+#cpufreq-set -c 0 -d 4.2GHz -g performance
 nodeos --data-dir $(pwd)/data --config-dir $(pwd)/config &> $(pwd)/logs/$(date +%Y-%m-%d_%H-%M-%S.log) & echo $! > $(pwd)/nodeos.pid
-taskset -p -c 0 $(cat nodeos.pid)
+#taskset -p -c 0 $(cat nodeos.pid)
