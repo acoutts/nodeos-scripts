9b41481ee7c08b8f8fcb19665dedf50518eca028
Andrew Coutts <andrewjohncoutts@gmail.com>
2019-04-25 19:47:05 +0000

    Update start.sh and config.ini

--- start.sh
+++ start.sh
@@ -3,9 +3,4 @@
 ./stop.sh
 echo -e "Starting Nodeos \n";
 
-#ulimit -n 65535
-#ulimit -s 64000
-
-#cpufreq-set -c 0 -d 4.2GHz -g performance
-nodeos --data-dir $(pwd)/data --config-dir $(pwd)/config &> $(pwd)/logs/$(date +%Y-%m-%d_%H-%M-%S.log) & echo $! > $(pwd)/nodeos.pid
-#taskset -p -c 0 $(cat nodeos.pid)
+nodeos --data-dir /eos/eos-blocks/data --config-dir $(pwd)/config &> $(pwd)/logs/$(date +%Y-%m-%d_%H-%M-%S.log) & echo $! > $(pwd)/nodeos.pid
