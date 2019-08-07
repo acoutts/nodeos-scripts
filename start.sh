a4d9ccbe0e59aea402493f90c004aee61b37e9a5
Andrew Coutts <andrewjohncoutts@gmail.com>
2019-08-07 12:40:55 +0000

    Updated config.ini for nodeos 1.8 and start.sh to point to zdata mount

--- start.sh
+++ start.sh
@@ -3,4 +3,4 @@
 ./stop.sh
 echo -e "Starting Nodeos \n";
 
-nodeos --data-dir /eos/eos-blocks/data --config-dir $(pwd)/config &> $(pwd)/logs/$(date +%Y-%m-%d_%H-%M-%S.log) & echo $! > $(pwd)/nodeos.pid
+nodeos --data-dir /zdata/eos-blocks/data --config-dir $(pwd)/config &> $(pwd)/logs/$(date +%Y-%m-%d_%H-%M-%S.log) & echo $! > $(pwd)/nodeos.pid
